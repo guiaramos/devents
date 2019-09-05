0a53e81d427cc0f76eab03321210231755dca45c
Guilherme <gui_aramos@outlook.com>
2019-09-05 17:01:07 +0900

    Add: Event form; Event list

--- src/components/event/eventList/EventListAttendee/index.jsx
+++ src/components/event/eventList/EventListAttendee/index.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react';
 
-export default class EventListAttendee extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class EventListAttendee extends Component {
+  render() {
+    const { attendee } = this.props;
+    return (
+      <List.Item>
+        <Image
+          as='a'
+          size='mini'
+          circular
+          src={attendee.photoURL}
+        />
+      </List.Item>
+    );
+  }
 }
 
-export default EventListAttendee
+export default EventListAttendee;
