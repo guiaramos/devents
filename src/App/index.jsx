c5a06379a91c08d95fe97a9425e62b242f3e0ce2
Guilherme <gui_aramos@outlook.com>
2019-09-05 15:49:08 +0900

    Add: Navigation bar

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import EventDashboard from '../components/event/EventDashBoard';
+import NavBar from '../components/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>DevEvents</h1>
-        <EventDashboard />
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
---
+++ src/components/event/eventList/EventListAttendee/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class EventListAttendee extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EventListAttendee
