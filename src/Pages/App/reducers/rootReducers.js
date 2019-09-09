65bcb6ab96966e77cf4cc8244d36f73120540121
Guilherme Ramos <guilhermeramos@Guilhermes-MacBook-Pro.local>
2019-09-09 18:26:04 +0900

    Start: implement redux-form on the app

--- src/Pages/App/reducers/rootReducers.js
+++ src/Pages/App/reducers/rootReducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import {reducer as FormReducer} from 'redux-form'
 import testReducer from '../../../components/testarea/testReduces';
 import eventReducer from '../../../components/event/eventReducer';
 
 // Adds the reducers to the store
 const rootReducer = combineReducers({
+  form: FormReducer,
   test: testReducer,
   events: eventReducer
 });
